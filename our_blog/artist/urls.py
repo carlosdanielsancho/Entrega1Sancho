0b54c8e8c523b0ce1a2398605476d363a3a1b9e2
carlosdanielsancho <carlosdanielsancho@gmail.com>
2022-10-29 09:51:07 -0300

    creación de algunas otras vistas y urls

--- our_blog/artist/urls.py
+++ our_blog/artist/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path("artist/<int:pk>/detail/", views.ArtistDetailView.as_view(), name="artist-detail"),
     path("artist/<int:pk>/update/", views.ArtistUpdateView.as_view(), name="artist-update"),
     path("artist/<int:pk>/delete/", views.ArtistDeleteView.as_view(), name="artist-delete"),
+    
+    # path("artists/", views.artists, name="artist-list"),
+    # path("artist/add/", views.artist_create, name="artist-add"),
+    # path('artist/<int:pk>/detail/', views.artist_detail, name='artist-detail'),
+    # path('artist/<int:pk>/update/', views.artist_update, name='artist-update'),
+    # path('artist/<int:pk>/delete/', views.artist_delete, name='artist-delete'),
 ]
